2d27836d6859ba2418ad8ef2dbaff6d0d530d430
cherriedy <huy5adv@gmail.com>
2025-03-31 00:45:04 +0700

    feat: integrate Hilt for dependency injection in BrandSelectionFragment and BrandSelectionViewModel, and enhance search functionality

--- app/src/main/java/com/optlab/banhangso/ui/brand/view/BrandSelectionFragment.java
+++ app/src/main/java/com/optlab/banhangso/ui/brand/view/BrandSelectionFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,152 +15,122 @@ import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.optlab.banhangso.data.repository.BrandRepository;
 import com.optlab.banhangso.ui.adapter.BrandSelectionAdapter;
 import com.optlab.banhangso.databinding.FragmentOptionSelectionBinding;
-import com.optlab.banhangso.factory.BrandSelectionViewModelFactory;
-import com.optlab.banhangso.data.model.Brand;
-import com.optlab.banhangso.data.repository.BrandRepository;
 import com.optlab.banhangso.ui.brand.viewmodel.BrandSelectionViewModel;
 
 import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
+import javax.inject.Inject;
+
+import dagger.hilt.android.AndroidEntryPoint;
 import timber.log.Timber;
 
+@AndroidEntryPoint
 public class BrandSelectionFragment extends Fragment {
+  public static final String KEY_CHECKED_POSITION = "checked_brand_position_key";
 
-    public static final String KEY_CHECKED_POSITION = "checked_brand_position_key";
+  @Inject protected BrandRepository repository;
 
-    private final BrandRepository repository = BrandRepository.getInstance();
+  private FragmentOptionSelectionBinding binding;
+  private BrandSelectionViewModel viewModel;
+  private BrandSelectionAdapter adapter;
 
-    private FragmentOptionSelectionBinding binding;
-    private BrandSelectionViewModel viewModel;
-    private BrandSelectionAdapter adapter;
+  @Nullable
+  @Override
+  public View onCreateView(
+      @NonNull LayoutInflater inflater,
+      @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    binding = FragmentOptionSelectionBinding.inflate(inflater, container, false);
+    return binding.getRoot();
+  }
 
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        binding = FragmentOptionSelectionBinding.inflate(inflater, container, false);
-        return binding.getRoot();
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    viewModel = new ViewModelProvider(this).get(BrandSelectionViewModel.class);
+    setupBrandListAdapter();
+    handleSearchView();
+  }
+
+  @Override
+  public void onDestroyView() {
+    super.onDestroyView();
+    binding = null;
+  }
+
+  /**
+   * This method is called when an item in the adapter is selected. It retrieves the checked
+   * position and sets it in the previous back stack entry's SavedStateHandle.
+   *
+   * @param checkedPosition The position of the selected item in the RecyclerView.
+   */
+  private void onItemSelected(int checkedPosition) {
+    if (checkedPosition != RecyclerView.NO_POSITION) {
+      NavController navController = NavHostFragment.findNavController(this);
+      NavBackStackEntry prevBackStackEntry = navController.getPreviousBackStackEntry();
+
+      if (prevBackStackEntry != null) {
+        prevBackStackEntry
+            .getSavedStateHandle() // Get the previous back stack entry's SavedStateHandle
+            .set(KEY_CHECKED_POSITION, checkedPosition); // Set back the checked position
+      } else {
+        Timber.e("Previous back stack entry is null.");
+      }
     }
+  }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+  /**
+   * Initializes and binds the BrandSelectionAdapter to the RecyclerView.
+   *
+   * <p>This method retrieves the brand ID from the arguments passed to this fragment and sets up
+   * the adapter with the initial checked position. It also observes the brand list from the
+   * ViewModel and updates the adapter when the brand list changes.
+   */
+  private void setupBrandListAdapter() {
+    // Get the arguments passed to this fragment, specifically the brand ID.
+    BrandSelectionFragmentArgs args = BrandSelectionFragmentArgs.fromBundle(requireArguments());
 
-        initViewModel();
+    // Initialize the RecyclerView adapter with a listener for item selection.
+    adapter = new BrandSelectionAdapter(this::onItemSelected);
 
-        setupBrandListAdapter();
+    // Set the initial checked position in the adapter based on the brand ID passed.
+    adapter.setCheckedPosition(repository.getPositionById(args.getBrandId()));
 
-        handleSearchView();
+    binding.listOption.setHasFixedSize(true);
+    binding.listOption.setAdapter(adapter);
 
-        viewModel.getCheckedPosition().observe(getViewLifecycleOwner(), position -> {
-            Toast.makeText(requireContext(), "Position: " + position, Toast.LENGTH_SHORT).show();
-        });
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
-
-    private void initViewModel() {
-        BrandSelectionViewModelFactory factory = new BrandSelectionViewModelFactory(repository);
-        viewModel = new ViewModelProvider(this, factory).get(BrandSelectionViewModel.class);
-    }
-
-    /**
-     * Initializes and binds the BrandSelectionAdapter to the RecyclerView.
-     *
-     * <p>This method sets up the adapter with an item click listener that updates the selected brand position in the ViewModel.
-     * It configures the RecyclerView once and observes the brand list from the repository, updating the adapter accordingly.
-     * If the brand list is empty or null, it logs an error and submits an empty list to clear any previous data.</p>
-     */
-    private void setupBrandListAdapter() {
-
-        BrandSelectionFragmentArgs args = BrandSelectionFragmentArgs.fromBundle(requireArguments());
-
-        // Initialize the adapter with an on-item-click listener that updates the ViewModel.
-        adapter = new BrandSelectionAdapter(checkedPosition -> {
-            if (checkedPosition != RecyclerView.NO_POSITION) {
-                // Find the NavController associated with this fragment.
-                NavController navController = NavHostFragment.findNavController(this);
-
-                // Get the previous back stack entry (ProductEditFragment).
-                NavBackStackEntry previousBackStackEntry = navController.getPreviousBackStackEntry();
-
-                // Send the data back to previous fragment (ProductEditFragment).
-                assert previousBackStackEntry != null;
-                previousBackStackEntry.getSavedStateHandle().set(KEY_CHECKED_POSITION, checkedPosition);
-            }
-        });
-
-        adapter.setCheckedPosition(repository.getPositionById(args.getBrandId()));
-
-        binding.listOption.setHasFixedSize(true);
-
-        // Bind the adapter to the `RecyclerView`.
-        binding.listOption.setAdapter(adapter);
-
-        // Observe the brand list from the repository.
-        repository.getBrands().observe(getViewLifecycleOwner(), brands -> {
-            // If brands list is null or empty, log an error and submit an empty list.
-            if (brands == null) {
-                Timber.e("Received an empty list of brand from repository.");
-                // Submit the list of brands to the adapter to display
+    // Observe the checked position in the ViewModel and update the adapter accordingly.
+    viewModel
+        .getBrands()
+        .observe(
+            getViewLifecycleOwner(),
+            brands -> {
+              if (brands == null || brands.isEmpty()) {
                 adapter.submitList(Collections.emptyList());
-            } else {
-                // Otherwise, submit the received list to the adapter.
+                Timber.e("Brand list is empty or null.");
+              } else {
                 adapter.submitList(brands);
-            }
+              }
+            });
+  }
+
+  /** Update the search query in the ViewModel when the text changes. */
+  private void handleSearchView() {
+    binding.svKeyword.setOnQueryTextListener(
+        new SearchView.OnQueryTextListener() {
+          @Override
+          public boolean onQueryTextSubmit(String query) {
+            return false;
+          }
+
+          @Override
+          public boolean onQueryTextChange(String newText) {
+            viewModel.setSearchQuery(newText); // Update the search query in the ViewModel
+            return true;
+          }
         });
-    }
-
-    private void handleSearchView() {
-        binding.svKeyword.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                getFilteredList(newText);
-                return true;
-            }
-        });
-    }
-
-    /**
-     * Filters the list of brands based on the provided keyword and updates the adapter.
-     *
-     * <p>If the keyword is empty, the full list of brands is displayed. Otherwise, it filters the list
-     * to include only those brands whose names contain the keyword (case-insensitive).</p>
-     *
-     * @param keyword The search keyword used to filter the brands.
-     */
-    private void getFilteredList(String keyword) {
-        // Retrieve the current list of brands from the ViewModel,
-        // if the list is null, use an empty list as the fallback
-        List<Brand> filterBrands = Optional.ofNullable(viewModel.getBrands().getValue())
-                // Map the non-null list to either the original or a filtered list based on the keyword
-                .map(brands -> keyword.isEmpty()
-                        // If the keyword is empty, return the original list of brands
-                        ? brands
-                        // Otherwise, filter the list to include only brands whose names contain the keyword
-                        : brands.stream()
-                        // Convert brand name to lowercase for a case-insensitive comparison
-                        .filter(brand -> brand.getName()
-                                .toLowerCase()
-                                .contains(keyword.toLowerCase()))
-                        // Collect the filtered brands into a new list
-                        .collect(Collectors.toList()))
-                // If the ViewModel's brand list is null, default to an empty list
-                .orElse(Collections.emptyList());
-
-        // Update the adapter with the filtered list, triggering a UI refresh.
-        adapter.submitList(filterBrands);
-    }
+  }
 }
--- app/src/main/java/com/optlab/banhangso/ui/brand/viewmodel/BrandSelectionViewModel.java
+++ app/src/main/java/com/optlab/banhangso/ui/brand/viewmodel/BrandSelectionViewModel.java
@@ -1,45 +1,71 @@
 package com.optlab.banhangso.ui.brand.viewmodel;
 
+import android.text.TextUtils;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.optlab.banhangso.data.model.Brand;
-import com.optlab.banhangso.data.repository.BrandRepository;
+import com.optlab.banhangso.data.repository.impl.BrandRepositoryImpl;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import javax.inject.Inject;
+
+import dagger.hilt.android.lifecycle.HiltViewModel;
+
+@HiltViewModel
 public class BrandSelectionViewModel extends ViewModel {
+  private final BrandRepositoryImpl repository;
+  private final MutableLiveData<List<Brand>> brandsSourceLiveData = new MutableLiveData<>();
+  private final MutableLiveData<String> searchQueryLiveData = new MutableLiveData<>("");
+  private final MediatorLiveData<List<Brand>> brandsLiveData = new MediatorLiveData<>();
 
-    private final BrandRepository repository;
-    private final MutableLiveData<List<Brand>> brands = new MutableLiveData<>();
-    private final MutableLiveData<Integer> checkedPosition = new MutableLiveData<>(RecyclerView.NO_POSITION);
+  @Inject
+  public BrandSelectionViewModel(@NonNull BrandRepositoryImpl repository) {
+    this.repository = repository;
+    // Observe the brands change from repository and update the list.
+    repository.getBrands().observeForever(brandsSourceLiveData::setValue);
 
-    public BrandSelectionViewModel(BrandRepository repository) {
-        this.repository = repository;
-        retrieveBrands();
+    // Set up the mediator to observe changes in the brands list and search query.
+    brandsLiveData.addSource(brandsSourceLiveData, unused -> updateBrands());
+    brandsLiveData.addSource(searchQueryLiveData, unused -> updateBrands());
+  }
+
+  /** Update the brands list based on the search query. If the query is empty, show all brands. */
+  private void updateBrands() {
+    List<Brand> updatedBrands = brandsSourceLiveData.getValue();
+    if (updatedBrands == null || updatedBrands.isEmpty()) return;
+
+    String query = searchQueryLiveData.getValue();
+    if (!TextUtils.isEmpty(query)) {
+      updatedBrands =
+          updatedBrands.stream()
+              .filter(brand -> brand.getName().toLowerCase().contains(query.toLowerCase()))
+              .collect(Collectors.toList());
     }
 
-    @Override
-    protected void onCleared() {
-        super.onCleared();
-        repository.getBrands().removeObserver(brands::setValue);
-    }
+    brandsLiveData.setValue(updatedBrands);
+  }
 
-    public LiveData<List<Brand>> getBrands() {
-        return brands;
-    }
+  @Override
+  protected void onCleared() {
+    repository.getBrands().removeObserver(brandsSourceLiveData::setValue);
+    brandsLiveData.removeSource(brandsSourceLiveData);
+    brandsLiveData.removeSource(searchQueryLiveData);
+    super.onCleared();
+  }
 
-    private void retrieveBrands() {
-        repository.getBrands().observeForever(brands::setValue);
-    }
+  public LiveData<List<Brand>> getBrands() {
+    return brandsLiveData;
+  }
 
-    public LiveData<Integer> getCheckedPosition() {
-        return checkedPosition;
-    }
-
-    public void setCheckedPosition(int position) {
-        checkedPosition.setValue(position);
-    }
+  public void setSearchQuery(@Nullable String query) {
+    searchQueryLiveData.setValue(query);
+  }
 }
