c53dd1eb1ecef2a4848a31c5fe0b742bd2e70211
cherriedy <huy5adv@gmail.com>
2025-03-31 00:45:17 +0700

    feat: refactor CategorySelectionFragment and CategorySelectionViewModel to use Hilt for dependency injection and improve search functionality

--- app/src/main/java/com/optlab/banhangso/ui/category/viewmodel/CategorySelectionFragment.java
+++ app/src/main/java/com/optlab/banhangso/ui/category/viewmodel/CategorySelectionFragment.java
@@ -17,132 +17,113 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.optlab.banhangso.ui.adapter.CategorySelectionAdapter;
 import com.optlab.banhangso.databinding.FragmentOptionSelectionBinding;
-import com.optlab.banhangso.factory.CategorySelectionViewModelFactory;
-import com.optlab.banhangso.data.model.Category;
-import com.optlab.banhangso.data.repository.CategoryRepository;
+import com.optlab.banhangso.data.repository.impl.CategoryRepositoryImpl;
 
 import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
+import javax.inject.Inject;
+
+import dagger.hilt.android.lifecycle.HiltViewModel;
 import timber.log.Timber;
 
+@HiltViewModel
 public class CategorySelectionFragment extends Fragment {
+  public static final String KEY_CHECKED_POSITION = "checked_category_position_key";
 
-    public static final String KEY_CHECKED_POSITION = "checked_category_position_key";
+  @Inject protected CategoryRepositoryImpl repository;
 
-    private final CategoryRepository repository = CategoryRepository.getInstance();
+  private FragmentOptionSelectionBinding binding;
+  private CategorySelectionViewModel viewModel;
+  private CategorySelectionAdapter adapter;
 
-    private FragmentOptionSelectionBinding binding;
-    private CategorySelectionViewModel viewModel;
-    private CategorySelectionAdapter adapter;
+  @Nullable
+  @Override
+  public View onCreateView(
+      @NonNull LayoutInflater inflater,
+      @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    binding = FragmentOptionSelectionBinding.inflate(inflater, container, false);
+    return binding.getRoot();
+  }
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        binding = FragmentOptionSelectionBinding.inflate(inflater, container, false);
-        return binding.getRoot();
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    viewModel = new ViewModelProvider(this).get(CategorySelectionViewModel.class);
+    setupCategoryListAdapter();
+    handleSearchView();
+  }
+
+  /**
+   * This method is called when an item in the adapter is selected. It retrieves the checked
+   * position and sets it in the previous back stack entry's SavedStateHandle.
+   *
+   * @param checkedPosition The position of the selected item in the RecyclerView.
+   */
+  private void onItemSelected(int checkedPosition) {
+    if (checkedPosition != RecyclerView.NO_POSITION) {
+      NavController navController = NavHostFragment.findNavController(this);
+      NavBackStackEntry prevBackStackEntry = navController.getPreviousBackStackEntry();
+
+      if (prevBackStackEntry != null) {
+        prevBackStackEntry
+            .getSavedStateHandle() // Get the previous back stack entry's SavedStateHandle
+            .set(KEY_CHECKED_POSITION, checkedPosition); // Set back the checked position
+      } else {
+        Timber.e("Previous back stack entry is null.");
+      }
     }
+  }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+  private void setupCategoryListAdapter() {
+    // Get the arguments passed to this fragment, specifically the category ID.
+    CategorySelectionFragmentArgs args =
+        CategorySelectionFragmentArgs.fromBundle(requireArguments());
 
-        initViewModel();
+    // Initialize the RecyclerView adapter with a listener for item selection.
+    adapter = new CategorySelectionAdapter(this::onItemSelected);
 
-        setupCategoryListAdapter();
+    // Set the initial checked position in the adapter based on the category ID passed.
+    adapter.setCheckedPosition(repository.getPositionById(args.getCategoryId()));
 
-        handleSearchView();
-    }
+    binding.listOption.setAdapter(adapter);
+    binding.listOption.setHasFixedSize(true);
 
-    private void setupCategoryListAdapter() {
-
-        CategorySelectionFragmentArgs args = CategorySelectionFragmentArgs.fromBundle(requireArguments());
-
-        adapter = new CategorySelectionAdapter(checkedPosition -> {
-            if (checkedPosition != RecyclerView.NO_POSITION) {
-                // Get the current NavController associated with the fragment.
-                NavController navController = NavHostFragment.findNavController(this);
-
-                // Find the previous back stack to send the data back.
-                NavBackStackEntry previousBackStackEntry = navController.getPreviousBackStackEntry();
-
-                // Send the data back to previous fragment (ProductEditFragment).
-                assert previousBackStackEntry != null;
-                previousBackStackEntry.getSavedStateHandle().set(KEY_CHECKED_POSITION, checkedPosition);
-            }
-        });
-
-        adapter.setCheckedPosition(repository.getPositionById(args.getCategoryId()));
-
-        binding.listOption.setAdapter(adapter);
-
-        binding.listOption.setHasFixedSize(true);
-
-        viewModel.getCategories().observe(getViewLifecycleOwner(), categories -> {
-            if (categories == null) {
-                Timber.e("Received an empty list of category from repository.");
+    // Observe the checked position in the ViewModel and update the adapter accordingly.
+    viewModel
+        .getCategoriesSourceLiveData()
+        .observe(
+            getViewLifecycleOwner(),
+            categories -> {
+              if (categories == null || categories.isEmpty()) {
                 adapter.submitList(Collections.emptyList());
-            } else {
+                Timber.e("Categories list is empty or null.");
+              } else {
                 adapter.submitList(categories);
-            }
+              }
+            });
+  }
+
+  @Override
+  public void onDestroyView() {
+    super.onDestroyView();
+    binding = null;
+  }
+
+  /** Update the search query in the ViewModel when text changes. */
+  private void handleSearchView() {
+    binding.svKeyword.setOnQueryTextListener(
+        new SearchView.OnQueryTextListener() {
+          @Override
+          public boolean onQueryTextSubmit(String query) {
+            return false;
+          }
+
+          @Override
+          public boolean onQueryTextChange(String newText) {
+            viewModel.setSearchQuery(newText); // Update the search query in the ViewModel
+            return true;
+          }
         });
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-
-        // Set binding to null to prevent memory leak
-        binding = null;
-    }
-
-    private void initViewModel() {
-        CategorySelectionViewModelFactory factory = new CategorySelectionViewModelFactory(repository);
-        viewModel = new ViewModelProvider(this, factory).get(CategorySelectionViewModel.class);
-    }
-
-    /**
-     * Add the query text listener to the search view.
-     */
-    private void handleSearchView() {
-        binding.svKeyword.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                getFilteredList(newText);
-                return true;
-            }
-        });
-    }
-
-    /**
-     * Collect the matching categories to the given keyword.
-     *
-     * <p>Retrieve the full list from view model, if it is null return an empty collection. If the keyword is empty,
-     * the full list of categories is displayed. Otherwise, the filtered list gathers category matched the keyword.</p>
-     *
-     * @param keyword Type the keyword contained in the finding category.
-     */
-    private void getFilteredList(String keyword) {
-        // Retrieve the categories which contains keyword. In case the initial list is null, return an empty list.
-        List<Category> filteredCategories = Optional.ofNullable(viewModel.getCategories().getValue())
-                // If the keyword is empty, return the retrieved list instead.
-                .map(categories -> keyword.isEmpty()
-                        ? categories
-                        : categories.stream()
-                        .filter(category -> category.getName()
-                                .toLowerCase()
-                                .contains(keyword.toLowerCase()))
-                        .collect(Collectors.toList()))
-                .orElse(Collections.emptyList());
-
-        // Submit the filtered list to the adapter for displaying.
-        adapter.submitList(filteredCategories);
-    }
+  }
 }
--- app/src/main/java/com/optlab/banhangso/ui/category/viewmodel/CategorySelectionViewModel.java
+++ app/src/main/java/com/optlab/banhangso/ui/category/viewmodel/CategorySelectionViewModel.java
@@ -1,47 +1,75 @@
 package com.optlab.banhangso.ui.category.viewmodel;
 
+import android.text.TextUtils;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.optlab.banhangso.data.model.Brand;
 import com.optlab.banhangso.data.model.Category;
-import com.optlab.banhangso.data.repository.CategoryRepository;
+import com.optlab.banhangso.data.repository.impl.CategoryRepositoryImpl;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import javax.inject.Inject;
+
+import dagger.hilt.android.lifecycle.HiltViewModel;
+
+@HiltViewModel
 public class CategorySelectionViewModel extends ViewModel {
+  private final CategoryRepositoryImpl repository;
+  private final MutableLiveData<List<Category>> categoriesSourceLiveData = new MutableLiveData<>();
+  private final MutableLiveData<String> searchQueryLiveData = new MutableLiveData<>("");
+  private final MediatorLiveData<List<Category>> categoriesLiveData = new MediatorLiveData<>();
 
-    private final CategoryRepository repository;
-    private final MutableLiveData<List<Category>> categories = new MutableLiveData<>();
-    private final MutableLiveData<Integer> checkedPosition = new MutableLiveData<>();
+  @Inject
+  public CategorySelectionViewModel(@NonNull CategoryRepositoryImpl repository) {
+    this.repository = repository;
+    // Observe the brands change from repository and update the list.
+    repository.getCategories().observeForever(categoriesSourceLiveData::setValue);
 
-    public CategorySelectionViewModel(@NonNull CategoryRepository repository) {
-        this.repository = repository;
+    // Set up the mediator to observe changes in the categories list and search query.
+    categoriesLiveData.addSource(categoriesSourceLiveData, unused -> updateCategories());
+    categoriesLiveData.addSource(searchQueryLiveData, unused -> updateCategories());
+  }
 
-        retrieveCategories();
+  /**
+   * Update the categories list based on the search query. If the query is empty, show all
+   * categories.
+   */
+  private void updateCategories() {
+    List<Category> updatedCategories = categoriesSourceLiveData.getValue();
+    if (updatedCategories == null || updatedCategories.isEmpty()) return;
+
+    String query = searchQueryLiveData.getValue();
+    if (!TextUtils.isEmpty(query)) {
+      updatedCategories =
+          updatedCategories.stream()
+              .filter(brand -> brand.getName().toLowerCase().contains(query.toLowerCase()))
+              .collect(Collectors.toList());
     }
 
-    @Override
-    protected void onCleared() {
-        super.onCleared();
-        repository.getCategories().removeObserver(categories::setValue);
-    }
+    categoriesLiveData.setValue(updatedCategories);
+  }
 
-    public LiveData<List<Category>> getCategories() {
-        return categories;
-    }
+  @Override
+  protected void onCleared() {
+    repository.getCategories().removeObserver(categoriesSourceLiveData::setValue);
+    categoriesLiveData.removeSource(categoriesSourceLiveData);
+    categoriesLiveData.removeSource(searchQueryLiveData);
+    super.onCleared();
+  }
 
-    public LiveData<Integer> getCheckedPosition() {
-        return checkedPosition;
-    }
+  public LiveData<List<Category>> getCategoriesSourceLiveData() {
+    return categoriesSourceLiveData;
+  }
 
-    public void setCheckedPosition(int position) {
-        checkedPosition.setValue(position);
-    }
-
-    private void retrieveCategories() {
-        repository.getCategories().observeForever(categories::setValue);
-    }
-
+  public void setSearchQuery(@Nullable String query) {
+    searchQueryLiveData.setValue(query);
+  }
 }
