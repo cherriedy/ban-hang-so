f861d3291de87cd810f7ae477b57401c6bad278c
cherriedy <huy5adv@gmail.com>
2025-04-04 22:43:14 +0700

    feat: add ValidatorModule to provide ProductValidator instance

---
+++ app/src/main/java/com/optlab/banhangso/di/ValidatorModule.java
@@ -0,0 +1,20 @@
+package com.optlab.banhangso.di;
+
+import android.content.Context;
+
+import com.optlab.banhangso.util.validator.ProductValidator;
+
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.ViewModelComponent;
+import dagger.hilt.android.qualifiers.ApplicationContext;
+
+@Module
+@InstallIn(ViewModelComponent.class)
+public class ValidatorModule {
+    @Provides
+    public static ProductValidator provideProductValidator(@ApplicationContext Context context) {
+        return new ProductValidator(context);
+    }
+}
