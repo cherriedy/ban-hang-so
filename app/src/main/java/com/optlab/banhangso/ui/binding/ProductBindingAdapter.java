e5fb022f7c333153e3fcce51ebc4e706681578b6
cherriedy <huy5adv@gmail.com>
2025-04-04 22:37:19 +0700

    feat: enhance ProductBindingAdapter to set brand and category names with null checks

--- app/src/main/java/com/optlab/banhangso/ui/binding/ProductBindingAdapter.java
+++ app/src/main/java/com/optlab/banhangso/ui/binding/ProductBindingAdapter.java
@@ -1,5 +1,6 @@
 package com.optlab.banhangso.ui.binding;
 
+import android.text.TextUtils;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -9,16 +10,28 @@ import com.optlab.banhangso.data.model.Brand;
 import com.optlab.banhangso.data.model.Category;
 
 public class ProductBindingAdapter {
+    /**
+     * Set brand name to TextView.
+     *
+     * @param view  TextView to set brand name
+     * @param brand Brand object
+     */
     @BindingAdapter("brand")
     public static void setBrand(@NonNull TextView view, Brand brand) {
-        if (brand != null) {
+        if (!TextUtils.isEmpty(brand.getName())) {
             view.setText(brand.getName());
         }
     }
 
+    /**
+     * Set category name to TextView.
+     *
+     * @param view     TextView to set category name
+     * @param category Category object
+     */
     @BindingAdapter("category")
     public static void setCategory(@NonNull TextView view, Category category) {
-        if (category != null) {
+        if (!TextUtils.isEmpty(category.getName())) {
             view.setText(category.getName());
         }
     }
