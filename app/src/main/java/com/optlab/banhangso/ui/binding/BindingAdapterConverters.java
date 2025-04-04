1913081a798fc0746cbc1d57cb75196f0b1d2f30
cherriedy <huy5adv@gmail.com>
2025-04-04 22:37:03 +0700

    feat: update BindingAdapterConverters to use boolean for status conversion

--- app/src/main/java/com/optlab/banhangso/ui/binding/BindingAdapterConverters.java
+++ app/src/main/java/com/optlab/banhangso/ui/binding/BindingAdapterConverters.java
@@ -1,24 +1,27 @@
 package com.optlab.banhangso.ui.binding;
 
-import androidx.annotation.NonNull;
 import androidx.databinding.InverseMethod;
-
 import com.optlab.banhangso.R;
-import com.optlab.banhangso.data.model.Product;
 
 public class BindingAdapterConverters {
-    @InverseMethod("buttonIdToStatus")
-    public static int statusToButtonId(@NonNull Product.ProductStatus status) {
-        if (status == Product.ProductStatus.IN_STOCK) {
-            return R.id.rb_in_stock;
-        }
-        return R.id.rb_out_stock;
-    }
+  /**
+   * Convert boolean status to button id for radio button group in layout.
+   *
+   * @param status boolean status of product
+   * @return R.id.rb_in_stock if status is true, R.id.rb_out_stock if status is false
+   */
+  @InverseMethod("buttonIdToStatus")
+  public static int statusToButtonId(boolean status) {
+    return status ? R.id.rb_in_stock : R.id.rb_out_stock;
+  }
 
-    public static Product.ProductStatus buttonIdToStatus(int buttonId) {
-        if (buttonId == R.id.rb_in_stock) {
-            return Product.ProductStatus.IN_STOCK;
-        }
-        return Product.ProductStatus.OUT_STOCK;
-    }
+  /**
+   * Convert button id to boolean status for radio button group in layout.
+   *
+   * @param buttonId button id of radio button group (R.id.rb_in_stock or R.id.rb_out_stock)
+   * @return true if button id is R.id.rb_in_stock, false if button id is R.id.rb_out_stock
+   */
+  public static boolean buttonIdToStatus(int buttonId) {
+    return buttonId == R.id.rb_in_stock;
+  }
 }
