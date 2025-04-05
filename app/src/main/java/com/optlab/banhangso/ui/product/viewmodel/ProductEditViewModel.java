2cc96ea8c652ad41127fa3b0ae0f8ca91170dac3
cherriedy <huy5adv@gmail.com>
2025-04-06 01:24:07 +0700

    feat: enhance ProductEditViewModel with update and delete handling, and button state management

--- app/src/main/java/com/optlab/banhangso/ui/product/viewmodel/ProductEditViewModel.java
+++ app/src/main/java/com/optlab/banhangso/ui/product/viewmodel/ProductEditViewModel.java
@@ -1,7 +1,11 @@
 package com.optlab.banhangso.ui.product.viewmodel;
 
+import android.text.TextUtils;
+import android.view.View;
+
 import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
@@ -17,6 +21,7 @@ import timber.log.Timber;
 @HiltViewModel
 public class ProductEditViewModel extends ViewModel {
     private final ProductRepository repository;
+    private final ProductValidator productValidator;
     private final MutableLiveData<Product> product = new MutableLiveData<>();
     private final MutableLiveData<String> nameErrorLiveData = new MutableLiveData<>();
     private final MutableLiveData<String> sellingPriceErrorLiveData = new MutableLiveData<>();
@@ -24,13 +29,45 @@ public class ProductEditViewModel extends ViewModel {
     private final MutableLiveData<String> discountPriceErrorLiveData = new MutableLiveData<>();
     private final MutableLiveData<String> descriptionErrorLiveData = new MutableLiveData<>();
     private final MutableLiveData<String> noteErrorLiveData = new MutableLiveData<>();
-    private final ProductValidator productValidator;
+    private final MediatorLiveData<Boolean> updateButtonStateLiveDate = new MediatorLiveData<>();
+    private final MutableLiveData<Boolean> isUpdatingLiveData = new MutableLiveData<>();
+    private final MutableLiveData<Boolean> updateResultLiveData = new MutableLiveData<>();
+    private final MutableLiveData<Boolean> isDeletingLiveData = new MutableLiveData<>();
+    private final MutableLiveData<Boolean> deleteResultLiveData = new MutableLiveData<>();
+    private final MutableLiveData<Boolean> isCreatingLiveData = new MutableLiveData<>();
+    private final MutableLiveData<Boolean> createResultLiveData = new MutableLiveData<>();
 
     @Inject
     public ProductEditViewModel(@NonNull ProductRepository repository,
                                 @NonNull ProductValidator productValidator) {
         this.repository = repository;
         this.productValidator = productValidator;
+        setUpdateButtonEnabled();
+    }
+
+    /**
+     * Sets up the MediatorLiveData to observe the error states of the input fields
+     */
+    private void setUpdateButtonEnabled() {
+        updateButtonStateLiveDate.addSource(nameErrorLiveData, unused -> updateButtonState());
+        updateButtonStateLiveDate.addSource(sellingPriceErrorLiveData, unused -> updateButtonState());
+        updateButtonStateLiveDate.addSource(purchasePriceErrorLiveData, unused -> updateButtonState());
+        updateButtonStateLiveDate.addSource(discountPriceErrorLiveData, unused -> updateButtonState());
+        updateButtonStateLiveDate.addSource(descriptionErrorLiveData, unused -> updateButtonState());
+        updateButtonStateLiveDate.addSource(noteErrorLiveData, unused -> updateButtonState());
+    }
+
+    /**
+     * Updates the state of the update button based on the error states of the input fields.
+     */
+    public void updateButtonState() {
+        boolean isEnabled = TextUtils.isEmpty(nameErrorLiveData.getValue())
+                && TextUtils.isEmpty(sellingPriceErrorLiveData.getValue())
+                && TextUtils.isEmpty(purchasePriceErrorLiveData.getValue())
+                && TextUtils.isEmpty(discountPriceErrorLiveData.getValue())
+                && TextUtils.isEmpty(descriptionErrorLiveData.getValue())
+                && TextUtils.isEmpty(noteErrorLiveData.getValue());
+        updateButtonStateLiveDate.setValue(isEnabled);
     }
 
     public void validateName(String name) {
@@ -71,13 +108,26 @@ public class ProductEditViewModel extends ViewModel {
         Timber.d("validateNote: %s", noteErrorLiveData.getValue());
     }
 
+    /**
+     * Loads a product by its ID. If the ID is empty, a new product is created. If the ID is not
+     * empty, the product is fetched from the repository.
+     *
+     * @param id The ID of the product to load
+     */
     public void loadProductById(String id) {
-        // Prevent reloading a new object if the current product has unsaved changes,
-        // which would cause data loss.
         Product currentProduct = product.getValue();
-        Product fetchedProduct = repository.getProductById(id);
-        if (currentProduct == null && fetchedProduct != null) {
-            product.setValue(fetchedProduct);
+        // Check if the product is already loaded.
+        if (currentProduct == null) {
+            if (id.isEmpty()) {
+                // If the ID is empty, create a new product.
+                product.setValue(new Product());
+            } else {
+                // If the ID is not empty, fetch the product from the repository.
+                Product fetchedProduct = repository.getProductById(id);
+                if (fetchedProduct != null) {
+                    product.setValue(fetchedProduct);
+                }
+            }
         }
     }
 
@@ -111,5 +161,86 @@ public class ProductEditViewModel extends ViewModel {
 
     public LiveData<String> getNoteError() {
         return noteErrorLiveData;
+    }
+
+    public LiveData<Boolean> getUpdateButtonState() {
+        return updateButtonStateLiveDate;
+    }
+
+    public LiveData<Boolean> getUpdateResult() {
+        return updateResultLiveData;
+    }
+
+    public LiveData<Boolean> getUpdateState() {
+        return isUpdatingLiveData;
+    }
+
+    public LiveData<Boolean> getDeleteResult() {
+        return deleteResultLiveData;
+    }
+
+    public LiveData<Boolean> getDeleteState() {
+        return isDeletingLiveData;
+    }
+
+    /**
+     * Called when the user clicks the "Finish" button to update the product.
+     *
+     * <p> This method retrieves the current product from the LiveData and updates it in the
+     * repository. It also sets the loading state and update result LiveData.
+     *
+     * @param view The view that was clicked
+     * @noinspection unused
+     */
+    public void onUpdateButtonClick(View view) {
+        Product currentProduct = product.getValue();
+        if (currentProduct != null) {
+            isUpdatingLiveData.setValue(true);
+            repository.updateProduct(
+                    currentProduct, isSuccessful -> {
+                        isUpdatingLiveData.setValue(false);
+                        updateResultLiveData.setValue(isSuccessful);
+                    });
+        } else {
+            Timber.e("Product is null when trying to update");
+        }
+    }
+
+    public void delete() {
+        Product currentProduct = product.getValue();
+        if (currentProduct != null) {
+            isDeletingLiveData.setValue(true);
+            repository.deleteProduct(
+                    currentProduct, isSuccessful -> {
+                        isDeletingLiveData.setValue(false);
+                        deleteResultLiveData.setValue(isSuccessful);
+                    });
+        } else {
+            Timber.e("Product is null when trying to delete");
+        }
+    }
+
+    public LiveData<Boolean> getCreateState() {
+        return isCreatingLiveData;
+    }
+
+    public LiveData<Boolean> getCreateResult() {
+        return createResultLiveData;
+    }
+
+    /**
+     * @noinspection unused
+     */
+    public void onCreateButtonClick(@NonNull View view) {
+        Product currentProduct = product.getValue();
+        if (currentProduct != null) {
+            isCreatingLiveData.setValue(true);
+            repository.createProduct(currentProduct, isSuccessful -> {
+                isCreatingLiveData.setValue(false);
+                createResultLiveData.setValue(isSuccessful);
+            });
+        } else {
+            Timber.e("Product is null when trying to create");
+        }
     }
 }
