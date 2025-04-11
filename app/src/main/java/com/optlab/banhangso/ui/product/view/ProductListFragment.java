0f6f1c34a1efc3b2657ef23040aa978d203ed26e
cherriedy <huy5adv@gmail.com>
2025-04-12 03:18:26 +0700

    feat: refactor ProductListFragment to streamline navigation actions and improve code readability

--- app/src/main/java/com/optlab/banhangso/ui/product/view/ProductListFragment.java
+++ app/src/main/java/com/optlab/banhangso/ui/product/view/ProductListFragment.java
@@ -32,16 +32,15 @@ import com.optlab.banhangso.ui.product.viewmodel.ProductSortSelectionViewModel;
 import com.optlab.banhangso.ui.product.viewmodel.ProductTabListViewModel;
 import com.optlab.banhangso.util.UserPreferenceManager;
 
+import dagger.hilt.android.AndroidEntryPoint;
+
 import java.util.EnumSet;
 
 import javax.inject.Inject;
 
-import dagger.hilt.android.AndroidEntryPoint;
-
 @AndroidEntryPoint
 public class ProductListFragment extends Fragment {
-    @Inject
-    protected CategoryRepository categoryRepository;
+    @Inject protected CategoryRepository categoryRepository;
     private FragmentProductListBinding binding;
     private ProductListViewModel productListViewModel;
     private ProductSortSelectionViewModel productSortSelectionViewModel;
@@ -58,12 +57,15 @@ public class ProductListFragment extends Fragment {
         productListAdapter =
                 new ProductListAdapter(
                         productId -> {
-                            NavDirections action = ProductTabsFragmentDirections.actionToEdit(productId, false);
+                            NavDirections action =
+                                    ProductTabsFragmentDirections.actionToEditFragment(
+                                            productId, false);
                             NavHostFragment.findNavController(this).navigate(action);
                         });
 
         // Setup adapter for RecyclerView of categories with callback to select category. If the
-        // position is NO_POSITION (-1), set the selection as null to trigger default sort. Otherwise,
+        // position is NO_POSITION (-1), set the selection as null to trigger default sort.
+        // Otherwise,
         // the position of selected category is set to ProductListViewModel.
         categoryTagSelectionAdapter =
                 new CategoryTagSelectionAdapter(
@@ -71,7 +73,8 @@ public class ProductListFragment extends Fragment {
                                 productListViewModel.setCategory(
                                         position == RecyclerView.NO_POSITION
                                                 ? null
-                                                : categoryRepository.getCategoryByPosition(position)));
+                                                : categoryRepository.getCategoryByPosition(
+                                                        position)));
 
         // Get the previous sort option from user preference and set it to ProductListViewModel.
         UserPreferenceManager userPreferenceManager = new UserPreferenceManager(requireContext());
@@ -116,9 +119,7 @@ public class ProductListFragment extends Fragment {
         productSortSelectionViewModel = getCurrentViewModel(ProductSortSelectionViewModel.class);
     }
 
-    /**
-     * Sets up the RecyclerView for displaying products.
-     */
+    /** Sets up the RecyclerView for displaying products. */
     private void setupProductRecyclerView() {
         binding.recyclerViewProduct.setLayoutManager(new LinearLayoutManager(requireContext()));
         EnumSet<LinearSpacingStrategy.Direction> directions =
@@ -127,7 +128,8 @@ public class ProductListFragment extends Fragment {
                         LinearSpacingStrategy.Direction.RIGHT,
                         LinearSpacingStrategy.Direction.TOP,
                         LinearSpacingStrategy.Direction.BOTTOM);
-        SpacingStrategy spacingStrategy = new LinearSpacingStrategy(requireContext(), 8, directions);
+        SpacingStrategy spacingStrategy =
+                new LinearSpacingStrategy(requireContext(), 8, directions);
         binding.recyclerViewProduct.addItemDecoration(new SpacingItemDecoration(spacingStrategy));
         binding.recyclerViewProduct.setHasFixedSize(true);
         binding.recyclerViewProduct.setAdapter(productListAdapter);
@@ -158,7 +160,6 @@ public class ProductListFragment extends Fragment {
         productTabListViewModel
                 .getToggleLayout()
                 .observe(getViewLifecycleOwner(), this::setupRecyclerViewLayout);
-
     }
 
     /**
@@ -176,25 +177,28 @@ public class ProductListFragment extends Fragment {
         if (isGrid) {
             GridLayoutManager gridLayoutManager = new GridLayoutManager(context, 2);
             SpacingStrategy spacingStrategy = new GridSpacingStrategy(context, 8);
-            binding.recyclerViewProduct.addItemDecoration(new SpacingItemDecoration(spacingStrategy));
+            binding.recyclerViewProduct.addItemDecoration(
+                    new SpacingItemDecoration(spacingStrategy));
             binding.recyclerViewProduct.setLayoutManager(gridLayoutManager);
             productListAdapter.setItemLayoutRes(R.layout.grid_item_product);
         } else {
             LinearLayoutManager linearLayoutManager = new LinearLayoutManager(context);
             SpacingStrategy spacingStrategy = new LinearSpacingStrategy(context, 8);
-            binding.recyclerViewProduct.addItemDecoration(new SpacingItemDecoration(spacingStrategy));
+            binding.recyclerViewProduct.addItemDecoration(
+                    new SpacingItemDecoration(spacingStrategy));
             binding.recyclerViewProduct.setLayoutManager(linearLayoutManager);
             productListAdapter.setItemLayoutRes(R.layout.list_item_horizontal_product);
         }
     }
 
-    /**
-     * Sets up the RecyclerView for displaying category tags.
-     */
+    /** Sets up the RecyclerView for displaying category tags. */
     private void setupCategoryRecyclerView() {
         EnumSet<LinearSpacingStrategy.Direction> directions =
-                EnumSet.of(LinearSpacingStrategy.Direction.LEFT, LinearSpacingStrategy.Direction.RIGHT);
-        SpacingStrategy spacingStrategy = new LinearSpacingStrategy(requireContext(), 8, directions);
+                EnumSet.of(
+                        LinearSpacingStrategy.Direction.LEFT,
+                        LinearSpacingStrategy.Direction.RIGHT);
+        SpacingStrategy spacingStrategy =
+                new LinearSpacingStrategy(requireContext(), 8, directions);
         binding.rvCategory.addItemDecoration(new SpacingItemDecoration(spacingStrategy));
         binding.rvCategory.setAdapter(categoryTagSelectionAdapter);
 
@@ -211,7 +215,7 @@ public class ProductListFragment extends Fragment {
      * @noinspection unused
      */
     public void onCreateButtonClick(@NonNull View view) {
-        NavDirections action = ProductTabsFragmentDirections.actionToEditFragment();
+        NavDirections action = ProductTabsFragmentDirections.actionToEditFragment("", true);
         NavHostFragment.findNavController(requireParentFragment()).navigate(action);
     }
 }
