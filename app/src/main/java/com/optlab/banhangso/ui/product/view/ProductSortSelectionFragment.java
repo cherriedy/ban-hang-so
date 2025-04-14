2e3f09c424260cf7be8e7d8fcdbcd6ba07fa5786
cherriedy <huy5adv@gmail.com>
2025-04-15 00:00:18 +0700

    feat: refactor ProductSortSelectionFragment to use shared ViewModel and improve navigation setup
    This commit refactors the `ProductSortSelectionFragment` to:
    
    -   Utilize a shared `ProductTabHostSharedViewModel` for communicating the selected sort option to the parent fragment.
    -   Initialize the adapter with a listener to handle sort option selection.
    -   Observe the selected sort option position from the `ProductSortSelectionViewModel` to update the adapter.
    -   Streamline ViewModel usage by obtaining shared view model from `nav_graph_product_tabs`
    -   Remove unnecessary `ProductSortOptionRepository` and `UserPreferenceManager`.

--- app/src/main/java/com/optlab/banhangso/ui/product/view/ProductSortSelectionFragment.java
+++ app/src/main/java/com/optlab/banhangso/ui/product/view/ProductSortSelectionFragment.java
@@ -1,28 +1,22 @@
 package com.optlab.banhangso.ui.product.view;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.optlab.banhangso.data.repository.ProductSortOptionRepository;
-import com.optlab.banhangso.ui.adapter.ProductSortSelectionAdapter;
-import com.optlab.banhangso.databinding.FragmentProductSortSelectionBinding;
-import com.optlab.banhangso.data.model.Product;
-import com.optlab.banhangso.data.repository.impl.ProductSortOptionRepositoryImpl;
-import com.optlab.banhangso.data.model.SortOption;
-import com.optlab.banhangso.util.UserPreferenceManager;
-import com.optlab.banhangso.ui.product.viewmodel.ProductSortSelectionViewModel;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavBackStackEntry;
+import androidx.navigation.fragment.NavHostFragment;
+
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
-
-import java.util.List;
-
-import javax.inject.Inject;
+import com.optlab.banhangso.R;
+import com.optlab.banhangso.databinding.FragmentProductSortSelectionBinding;
+import com.optlab.banhangso.ui.adapter.ProductSortSelectionAdapter;
+import com.optlab.banhangso.ui.product.viewmodel.ProductSortSelectionViewModel;
+import com.optlab.banhangso.ui.product.viewmodel.ProductTabHostSharedViewModel;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
@@ -35,73 +29,66 @@ import dagger.hilt.android.AndroidEntryPoint;
  */
 @AndroidEntryPoint
 public class ProductSortSelectionFragment extends BottomSheetDialogFragment {
-  @Inject protected ProductSortOptionRepository repository;
-  private FragmentProductSortSelectionBinding binding;
-  private ProductSortSelectionViewModel viewModel;
-  private ProductSortSelectionAdapter adapter;
+    private FragmentProductSortSelectionBinding binding;
+    private ProductSortSelectionViewModel viewModel;
+    private ProductSortSelectionAdapter adapter;
+    private ProductTabHostSharedViewModel tabHostSharedViewModel;
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setupNavigation();
-    setupRecyclerView();
-  }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initViewModels();
+        initAdapter();
+    }
 
-  private void setupNavigation() {
-    // NavController navController = NavHostFragment.findNavController(this);
-    // // Get the previous back stack entry to retrieve the ViewModel.
-    // NavBackStackEntry prevBackStackEntry = navController.getPreviousBackStackEntry();
-    // if (prevBackStackEntry != null) {
-    //   viewModel =
-    //       new ViewModelProvider(prevBackStackEntry).get(ProductSortSelectionViewModel.class);
-    //   new ViewModelProvider(prevBackStackEntry).get(ProductSortSelectionViewModel.class);
-    // } else {
-    //   Timber.e("Previous back stack entry is null. Cannot retrieve ViewModel.");
-    // }
+    private void initAdapter() {
+        // Initialize the adapter with a listener to handle sort option selection events.
+        adapter =
+                new ProductSortSelectionAdapter(
+                        sortOption -> {
+                            // Set the selected sort option in the ViewModel to update the UI.
+                            viewModel.setSelectedSortOptionPosition(sortOption);
 
-    viewModel =
-            new ViewModelProvider(requireParentFragment()).get(ProductSortSelectionViewModel.class);
-  }
+                            // Set the selected sort option in the shared ViewModel to communicate
+                            // with the parent fragment.
+                            tabHostSharedViewModel.setSelectedSortOption(sortOption);
+                        });
 
-  @Override
-  public View onCreateView(
-      @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    binding = FragmentProductSortSelectionBinding.inflate(inflater, container, false);
-    return binding.getRoot();
-  }
+        adapter.submitList(viewModel.getSortOptions()); // Set the initial list of sort options
+    }
 
-  @Override
-  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-    super.onViewCreated(view, savedInstanceState);
-    binding.rvSortSelection.setHasFixedSize(true);
-    binding.rvSortSelection.setAdapter(adapter);
-  }
+    private void initViewModels() {
+        viewModel = new ViewModelProvider(this).get(ProductSortSelectionViewModel.class);
 
-  /**
-   * Sets up the RecyclerView with a list of sort options.
-   *
-   * <p>Sets up the RecyclerView with a list of sort options. It retrieves the saved sort option
-   * from UserPreferenceManager and the sort option list from the ViewModel. The selected sort
-   * option is saved in UserPreferenceManager and set in the ViewModel when a sort option is
-   * clicked.
-   */
-  private void setupRecyclerView() {
-    // Retrieve the saved sort option from UserPreferenceManager.
-    UserPreferenceManager userPreferenceManager = new UserPreferenceManager(requireContext());
-    SortOption<Product.SortField> savedSortOption = userPreferenceManager.getSortOption();
+        NavBackStackEntry productTabsEntry =
+                NavHostFragment.findNavController(this)
+                        .getBackStackEntry(R.id.nav_graph_product_tabs);
+        tabHostSharedViewModel =
+                new ViewModelProvider(productTabsEntry).get(ProductTabHostSharedViewModel.class);
+    }
 
-    // Retrieve the sort option list from ViewModel, throwing an exception whether it is null.
-    List<SortOption<Product.SortField>> sortOptions = viewModel.getSortOptions();
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentProductSortSelectionBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
 
-    adapter =
-        new ProductSortSelectionAdapter(
-            sortOptions, // Submit the list of sort options to the adapter.
-            sortOption -> { // Set up the click listener for each sort option.
-              userPreferenceManager.saveSortOption(sortOption); // Save the selected sort option.
-              viewModel.setSortOption(sortOption); // Set the selected sort option in ViewModel.
-            });
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setupRecyclerView();
+        observeViewModel();
+    }
 
-    // Set the checked position in the adapter based on the saved sort option.
-    adapter.setCheckedPosition(repository.getPosition(savedSortOption));
-  }
+    private void setupRecyclerView() {
+        binding.rvSortSelection.setHasFixedSize(true);
+        binding.rvSortSelection.setAdapter(adapter);
+    }
+
+    private void observeViewModel() {
+        viewModel
+                .getSelectedSortOptionPosition()
+                .observe(getViewLifecycleOwner(), adapter::setCheckedPosition);
+    }
 }
