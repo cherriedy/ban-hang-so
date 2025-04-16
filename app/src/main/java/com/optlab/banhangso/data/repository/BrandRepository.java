ab482e6b859061979dd199222c8028cc559e503a
cherriedy <huy5adv@gmail.com>
2025-04-16 23:29:15 +0700

    feat: add create and update functionalities to BrandRepository
    This commit introduces the ability to create and update brands within the `BrandRepository`.
    - Implemented `createBrand` method in `BrandRepositoryImpl` to add new brands to Firestore.
    - Implemented `updateBrand` method in `BrandRepositoryImpl` to update existing brands in Firestore.
    - Modified `retrieveData` in `BrandRepositoryImpl` to get data from the brand collection path.
    - Added the brand collection path constant.
    - Added `createBrand`, `updateBrand` and `deleteBrand` methods in the `BrandRepository` interface.
    - Added callback to the `createBrand`, `updateBrand` and `deleteBrand` methods.
    - Updated logging to be more specific.

--- app/src/main/java/com/optlab/banhangso/data/repository/BrandRepository.java
+++ app/src/main/java/com/optlab/banhangso/data/repository/BrandRepository.java
@@ -5,6 +5,7 @@ import androidx.lifecycle.LiveData;
 import com.optlab.banhangso.data.model.Brand;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 public interface BrandRepository {
     LiveData<List<Brand>> getBrands();
@@ -14,4 +15,10 @@ public interface BrandRepository {
     Brand getBrandByPosition(int position);
 
     int getPositionById(String id);
+
+    void updateBrand(Brand brand, Consumer<Boolean> callback);
+
+    void deleteBrand(Brand brand, Consumer<Boolean> callback);
+
+    void createBrand(Brand brand, Consumer<Boolean> callback);
 }
