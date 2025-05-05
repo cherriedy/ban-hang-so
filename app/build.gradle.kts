fb5856fa00c829a5d9f5d640d18cf4a7aab8dbb5
cherriedy <huy5adv@gmail.com>
2025-05-05 23:44:25 +0700

    feat: refactor build.gradle.kts to use KSP, update dependencies, and adjust build configurations
    - Migrate from `kotlin-kapt` to `com.google.devtools.ksp` for annotation processing.
    - Update `minSdk` from 34 to 30.
    - Add `ksp(libs.room.compiler)` for Kotlin Symbol Processing.
    - Introduce `ksp(libs.hilt.android.compiler)` and  `ksp(libs.lombok)` for Hilt and Lombok.
    - Add dependencies for legacy support, lifecycle, `com.google.gms.google.services.gradle.plugin`, Facebook login, credentials, Lottie, CircleImageView.
    - Remove unnecessary `kapt` block.
    - Update and improve build configuration.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     id("androidx.navigation.safeargs")
     alias(libs.plugins.kotlin.android)
-    id("kotlin-kapt")
+    id("com.google.devtools.ksp")
     id("com.google.dagger.hilt.android")
     alias(libs.plugins.google.gms.google.services)
 }
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.optlab.banhangso"
-        minSdk = 34
+        minSdk = 30
         //noinspection ExpiredTargetSdkVersion
         targetSdk = 30
         versionCode = 1
@@ -46,36 +46,65 @@ android {
 }
 
 dependencies {
-    // Import the BoM for the Firebase platform
-    implementation(platform(libs.firebase.bom))
 
+    implementation(libs.legacy.support.v4)
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.viewmodel.ktx)
+    // Kotlin Symbol Processing
+    ksp(libs.room.compiler)
+
+    // Hilt dependencies
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+
+    // Lombok dependencies
+    compileOnly(libs.lombok)
+    ksp(libs.lombok)
+
+    // Base dependencies
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.constraintlayout)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
-    implementation(libs.glide)
-    implementation(libs.activity)
-    implementation(libs.timber)
     implementation(libs.core.ktx)
     implementation(libs.recyclerview.selection)
-    implementation(libs.gson)
+    implementation(libs.activity)
+
+
+    // Glide dependency
+    implementation(libs.glide)
+
+    // Timber dependency for logging
+    implementation(libs.timber)
+
+    // Firebase dependencies for authentication and Firestore
+    implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.firestore)
-    implementation(libs.lottie)
-    implementation(libs.circleimageview)
     implementation(libs.firebase.auth)
+    // Google Play Services dependencies for authentication
+    implementation(libs.play.services.auth)
+    implementation(libs.com.google.gms.google.services.gradle.plugin)
+    // Facebook SDK dependencies for login
+    implementation(libs.facebook.login)
+
+    // Credentials dependencies for Google Play Services
     implementation(libs.credentials)
     implementation(libs.credentials.play.services.auth)
+
     implementation(libs.googleid)
-    implementation(libs.com.google.gms.google.services.gradle.plugin)
-    implementation(libs.play.services.auth)
+    // Gson for JSON serialization/deserialization
+    implementation(libs.gson)
+
+    // Lottile dependency for animations
+    implementation(libs.lottie)
+
+    // CircleImageView dependency for displaying circular images
+    implementation(libs.circleimageview)
+
+    // Junit dependencies for testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
+    // Espresso dependencies for UI testing
     androidTestImplementation(libs.espresso.core)
-    implementation(libs.hilt.android)
-    kapt(libs.hilt.android.compiler)
-}
-
-kapt {
-    correctErrorTypes = true
 }
