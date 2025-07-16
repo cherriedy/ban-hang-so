da839f72aaa05b41050c01ea7e46359483c72d80
cherriedy <huy5adv@gmail.com>
2025-07-17 00:58:13 +0700

    refactor: streamline application class documentation and formatting

--- app/src/main/java/com/optlab/banhangso/BanHangSo.java
+++ app/src/main/java/com/optlab/banhangso/BanHangSo.java
@@ -1,14 +1,10 @@
 package com.optlab.banhangso;
 
 import android.app.Application;
-
 import com.optlab.banhangso.internal.utilities.Logging;
-
 import dagger.hilt.android.HiltAndroidApp;
 
-/**
- * Application class for BanHangSo app.
- */
+/** Application class for BanHangSo app. */
 @HiltAndroidApp
 public class BanHangSo extends Application {
 
